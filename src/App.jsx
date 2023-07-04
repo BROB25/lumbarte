7ca2df76fb617b951a285ed2b99cbe408683a2ef
Brandon Roberts <bbrenterprise25@gmail.com>
2023-07-04 16:20:01 -0500

    sup thot

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Finna get tha bag nigga
+          Finna get tha bag nigga!!
         </p>
       </div>
       <p className="read-the-docs">
